025cec3c92e6041a82edf1cd616ac4a4b79b3a63
devrawr <rawrdev@protonmail.com>
2022-05-22 18:51:59 +0200

    constructor without builtFile/parentDirectory parameters

--- parser/src/main/kotlin/io/desolve/parser/ParsedProject.kt
+++ parser/src/main/kotlin/io/desolve/parser/ParsedProject.kt
@@ -3,6 +3,7 @@ package io.desolve.parser
 import io.desolve.data.DependencyData
 import java.io.File
 import java.io.FileWriter
+import java.lang.IllegalStateException
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -10,12 +11,19 @@ class ParsedProject(
     val groupId: String,
     val artifactId: String,
     val version: String,
-    val builtFile: File,
-    val parentDirectory: File
+    val builtFile: File?,
+    val parentDirectory: File?
 )
 {
+    constructor(groupId: String, artifactId: String, version: String) : this(groupId, artifactId, version, null, null)
+
     fun generateDirectory(): File
     {
+        if (parentDirectory == null || builtFile == null)
+        {
+            throw IllegalStateException("Unable to call #generateDirectory() from ParsedObject without builtFile/parentDirectory fields.")
+        }
+
         val fileName = "${artifactId}-${version}"
         val data = DependencyData(
             groupId, artifactId, version, "${fileName}.jar", parentDirectory
