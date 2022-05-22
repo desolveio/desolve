4080363d283b3cc9126f86a8241ccb5687730fba
GrowlyX <growlyxdev@gmail.com>
2022-05-22 18:01:16 -0400

    Add maven publication stuff

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,8 @@ plugins {
 }
 
 allprojects {
-    group = "io.github.devrawr.repository"
-    version = "1.0-SNAPSHOT"
+    group = "io.desolve.repository"
+    version = "1.0"
 
     apply(plugin = "kotlin")
     apply(plugin = "maven-publish")
@@ -21,5 +21,25 @@ allprojects {
     dependencies {
         implementation("com.github.patrickzondervan:depenject:42af9d6a0b")
         testImplementation(kotlin("test"))
+    }
+
+    publishing {
+        repositories {
+            maven {
+                name = "desolve"
+                url = uri("${property("artifactory_contextUrl")}/gradle-release")
+
+                credentials {
+                    username = property("artifactory_user") as String
+                    password = property("artifactory_password") as String
+                }
+            }
+        }
+
+        publications {
+            register("mavenJava", MavenPublication::class) {
+                from(components["java"])
+            }
+        }
     }
 }
