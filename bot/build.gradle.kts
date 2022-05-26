39dee410e5454f80dddb0b8a9da1c2d44c59ac75
GrowlyX <growlyxdev@gmail.com>
2022-05-25 20:22:31 -0400

    Remove unnecessary depenject dependency

--- bot/build.gradle.kts
+++ bot/build.gradle.kts
@@ -9,9 +9,10 @@ repositories {
 dependencies {
     implementation(project(":common"))
     implementation(project(":parser"))
+
     implementation("com.github.patrickzondervan:file-update-watcher:0fd0e41d9d")
-    implementation("com.github.patrickzondervan:depenject:42af9d6a0b")
     implementation("dev.kord:kord-core:0.8.0-M8")
-    implementation("org.slf4j:slf4j-api:1.7.5")
-    implementation("org.slf4j:slf4j-simple:1.7.5")
+
+    implementation("org.slf4j:slf4j-api:1.7.36")
+    implementation("org.slf4j:slf4j-simple:1.7.36")
 }
